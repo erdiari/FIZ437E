b42b7908ac82915baf0a564b9b179cd0ee6378a3
erdiari <merdiari@gmail.com>
2021-12-09 19:13:58 +0300

    HW3: Tried learning from colored data

--- HW3/colored_mnist.py
+++ HW3/colored_mnist.py
@@ -1,24 +1,38 @@
 import numpy as np
+from torch.utils.data import Dataset
+from torch import from_numpy
 
 class coloredMNIST(Dataset):
     def __init__(self, data_dir, test=False, transform=None, target_transform=None):
         self.transform = transform
         self.target_transform = target_transform
-        if test:
-            self.data = np.load(data_dir + 'numpy_train_data.npy')
-            self.target = np.load(data_dir + 'numpy_train_label.npy')
+        self.is_test = test
+
+        if self.is_test:
+            self.data = from_numpy(np.load(data_dir + 'numpy_test_data.npz')['arr_0'])
+            self.target = from_numpy(np.load(data_dir + 'numpy_test_label.npz')['arr_0'])
         else:
-            self.data = np.load(data_dir + 'numpy_test_data.npy')
-            self.target = np.load(data_dir + 'numpy_test_label.npy')
+            self.data = from_numpy(np.load(data_dir + 'numpy_train_data.npz')['arr_0'])
+            self.target = from_numpy(np.load(data_dir + 'numpy_train_label.npz')['arr_0'])
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__ (self, idx):
-        data = self.data[idx]
-        target = self.target[idx]
         if self.transform:
-            data = self.transform(data)
+            data = self.transform(data)[idx]
+        else:
+            data = self.data[idx]
         if self.target_transform:
-            target = self.target_transform(data)
+            target = self.transform(data)[idx]
+        else:
+            target = self.target[idx]
         return data, target
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    cmnist_train = coloredMNIST('HW3/data/colored_MNIST/',test=False)
+    cmnist_test = coloredMNIST('HW3/data/colored_MNIST/',test=True)
+    print(cmnist_train.__len__())
+    plt.imshow(cmnist_test.__getitem__(277)[0])
+    plt.show()
